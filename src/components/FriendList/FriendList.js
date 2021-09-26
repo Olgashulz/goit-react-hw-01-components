f0e7ba5d75b83f539effb5848032bce7028b9fa9
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-09-26 18:31:52 +0300

    add prettier

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -1,20 +1,20 @@
 import PropTypes from 'prop-types';
 import styles from './FriendList.module.css';
 
-export default function FriendList ( { friends }) {
-    return (<ul className={styles.list}>
-     {friends.map((item) => (
-        <li className={styles.item} key={ item.id }>
+export default function FriendList({ friends }) {
+  return (<ul className={styles.list}>
+    {friends.map((item) => (
+      <li className={styles.item} key={item.id}>
         <span className={item.isOnline ? styles.online : styles.ofline}>{item.isOnline}</span>
         <img className={styles.avatar} src={item.avatar} alt={item.name} width="48" />
         <p className={styles.name}>{item.name}</p>
       </li>
-     
+
     ))}
-</ul>)
+  </ul>)
 }
 
 
 FriendList.propTypes = {
-    friends: PropTypes.array.isRequired,  
-  }
+  friends: PropTypes.array.isRequired,
+}
