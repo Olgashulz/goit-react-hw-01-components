ac33b5a8db7c3e766e77cdb941bb11161b7b2b33
Olgashulz <41305602+Olgashulz@users.noreply.github.com>
2021-09-26 18:51:03 +0300

    prettier.json

--- src/components/FriendList/FriendList.js
+++ src/components/FriendList/FriendList.js
@@ -2,19 +2,26 @@ import PropTypes from 'prop-types';
 import styles from './FriendList.module.css';
 
 export default function FriendList({ friends }) {
-  return (<ul className={styles.list}>
-    {friends.map((item) => (
-      <li className={styles.item} key={item.id}>
-        <span className={item.isOnline ? styles.online : styles.ofline}>{item.isOnline}</span>
-        <img className={styles.avatar} src={item.avatar} alt={item.name} width="48" />
-        <p className={styles.name}>{item.name}</p>
-      </li>
-
-    ))}
-  </ul>)
+  return (
+    <ul className={styles.list}>
+      {friends.map(item => (
+        <li className={styles.item} key={item.id}>
+          <span className={item.isOnline ? styles.online : styles.ofline}>
+            {item.isOnline}
+          </span>
+          <img
+            className={styles.avatar}
+            src={item.avatar}
+            alt={item.name}
+            width="48"
+          />
+          <p className={styles.name}>{item.name}</p>
+        </li>
+      ))}
+    </ul>
+  );
 }
-
 
 FriendList.propTypes = {
   friends: PropTypes.array.isRequired,
-}
+};
